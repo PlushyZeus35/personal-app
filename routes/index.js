1f4fb7ae87d3f692471f737e02263cb67aeba79a
Borja <0019746@viewnext.com>
2022-11-26 21:24:02 +0100

    authetication and UI improvements

--- routes/index.js
+++ routes/index.js
@@ -1,20 +1,29 @@
 var express = require('express');
 const { isLoggedIn } = require('../helpers/Authentication');
+const UserListener = require('../helpers/UserListener');
 const WeightListener = require('../helpers/WeightListener');
+const Crypto = require('../helpers/Crypto');
 var router = express.Router();
 
 /* GET Index page. */
 router.get('/', isLoggedIn,async (req, res) => {
+    const actualUser = req.user;
     console.log(req.user);
     const userWeightData = await WeightListener.getUsersWeights(req.user.id);
     console.log(userWeightData);
-    res.render('weight',{userWeightData});
+    res.render('weight',{userWeightData, actualUser});
 })
 
 /* GET Login page. */
 router.get('/login', (req, res) => {
     console.log(req.user);
     res.render('login');
+})
+
+/* GET Login page. */
+router.get('/registro', (req, res) => {
+    console.log(req.user);
+    res.render('register');
 })
 
 router.post('/weights', isLoggedIn, async (req, res) => {
